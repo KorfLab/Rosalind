819ac34e75a48afb54752510d69f59310dddea4a
kmarnott <kmarnott@ucdavis.edu>
2016-12-08 12:19:06 -0800

    Update dna_kat.pl

--- DNA/dna_kat.pl
+++ DNA/dna_kat.pl
@@ -1,20 +1,16 @@
 #!/usr/bin/perl
-#Rosalind_dna.pl by Kat
+#dna_kat.pl by Kat
 #Rosalind DNA Challenge
 use warnings; use strict;
 
 
-die "usage: ros_dna.pl <dna str>\n" unless @ARGV == 1;
+die "usage: dna_kat.pl <dna str>\n" unless @ARGV == 1;
 
 #variable to hold
 my $dna = $ARGV[0];
 
-#nucleotide counters, initialize to zero
-my ($a, $g, $t, $c) = (0, 0, 0, 0);
-$a = ($dna =~ tr/Aa/Aa/);
-$g = ($dna =~ tr/Gg/Gg/);
-$t = ($dna =~ tr/Tt/Tt/);
-$c = ($dna =~ tr/Cc/Cc/);
-print "A C G T: $a $c $g $t\n";
-
-
+my $a = ($dna =~ tr/Aa/Aa/);
+my $g = ($dna =~ tr/Gg/Gg/);
+my $t = ($dna =~ tr/Tt/Tt/);
+my $c = ($dna =~ tr/Cc/Cc/);
+print "$a $c $g $t\n";
