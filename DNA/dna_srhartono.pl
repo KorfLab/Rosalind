26d64705d3476313e50079c190a5ba8cef08b884
stella <srhartono@ucdavis.edu>
2016-12-08 12:22:15 -0800

    Update dna_srhartono.pl
    changed so it print in order A C G T and only A C G T. Changed usage to file input.

--- DNA/dna_srhartono.pl
+++ DNA/dna_srhartono.pl
@@ -14,7 +14,10 @@ my @nuc = split("", $DNA);
 for (my $i = 0; $i < @nuc; $i++) {
 	$data{$nuc[$i]} ++;
 }
-foreach my $nuc (sort keys %data) {
-	print "$data{$nuc} ";
+@nuc = qw(A C G T);
+for (my $i = 0; $i < @nuc; $i++) {
+	my $nuc = $nuc[$i];
+	my $count = defined $data{$nuc} ? $data{$nuc} : 0;
+	my $end = $i == @nuc - 1 ? "\n" : " ";
+	print "$count$end";
 }
-print "\n";
