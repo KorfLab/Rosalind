ec4867f92719539579be5fd7d099cfd04e8c1aa4
joydoong <jdoong@ucdavis.edu>
2016-12-15 04:36:14 -0800

    Load data from file

--- DNA/dna_joy.pl
+++ DNA/dna_joy.pl
@@ -3,12 +3,22 @@
 use strict; 
 use warnings;
 
-die "usage: dna_joy.pl <DNA input>\n" unless @ARGV == 1;
+die "usage: dna_joy.pl <DNA input file in Download folder>\n" unless @ARGV == 1;
 
-my ($DNA) = @ARGV;
+my $DNA; 
+
+open(my $in, "</Users/s130963/Downloads/$ARGV[0]") or die "error reading $ARGV[0]";
+while (<$in>) {
+	chomp;
+	$DNA = $DNA.$_;
+}
+close $in;
+
 my $A_count = ($DNA =~ tr/A/A/);
 my $T_count = ($DNA =~ tr/T/T/);
 my $C_count = ($DNA =~ tr/C/C/);
 my $G_count = ($DNA =~ tr/G/G/);
 
-print "$A_count $C_count $G_count $T_count\n";
+open(my $out, ">output.txt") or die "error writing output";
+print $out "$A_count $C_count $G_count $T_count\n";
+close $out;
