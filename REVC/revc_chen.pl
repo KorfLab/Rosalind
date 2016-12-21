e4a28fcf641e410b68293fa843bb58cba322aa90
xuchenidea <chnxu@ucdavis.edu>
2016-12-21 12:03:05 -0800

    Add files via upload

--- REVC/revc_chen.pl
+++ REVC/revc_chen.pl
@@ -1,11 +1,13 @@
-#!/usr/bin/perl -w
-use strict;
+#!/usr/bin/perl 
+# revc_chen.pl by chen
+use strict;  use warnings;
 
-open IN,"<$ARGV[0]";
-while (<IN>)  {
+die "usage: revc_chen.pl <file>" unless @ARGV == 1;
+
+while (<>)  {
 my $reverse_seq = reverse $_;
 # print "$reverse_seq\n";
 $reverse_seq =~ tr/ATCG/TAGC/;
 print "$reverse_seq\n";
 }
-close IN;  
+
