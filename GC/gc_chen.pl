ef6477a70d83e1e3e3cfe44b0d825cf40e640124
xuchenidea <chnxu@ucdavis.edu>
2017-01-08 19:28:34 -0800

    Add files via upload

--- GC/gc_chen.pl
+++ GC/gc_chen.pl
@@ -11,20 +11,22 @@ my $G_count;
 my $GC_count;
 my $seq;
 my %hash = {};
+my $max = 0;
+my $number;
+
 $/ = ">";<IN>;
 while (<IN>) {
         chomp;
 	my @id = (split /\n/,$_,2);      
-        #       print "$id[1]\n";
 	$C_count = $id[1] =~ tr/C/C/;
 	$G_count = $id[1] =~ tr/G/G/;
         $GC_count = $C_count+$G_count; 
         $GC_intent =int(100000000*$GC_count/length $id[1])/1000000;
-	$hash{$GC_intent} = $id[0];
+	if ($max <$GC_intent) {
+		$max = $GC_intent; $number = $id[0];
+	} 
 }
-my $max_gc = pop sort values $GC_intent;
-my $max_value = $hash{$max_gc};   
-print "$max_gc $max_value";
+print "$number\n$max\n";
 close IN;
 
  
