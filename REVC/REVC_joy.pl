18d5e9a7c3535a5da9d69b4c60590377d74f7d66
joydoong <jdoong@ucdavis.edu>
2017-01-10 23:26:24 -0800

    find reverse compliment of a DNA sequence

---
+++ REVC/REVC_joy.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+# REVC_joy.pl
+use strict; 
+use warnings;
+
+die "usage: REVC_joy.pl <DNA input file in Download folder>\n" unless @ARGV == 1;
+
+my $DNA; 
+
+open(my $in, "</Users/s130963/Downloads/$ARGV[0]") or die "error reading $ARGV[0]";
+while (<$in>) {
+	chomp;
+	$DNA = $DNA.$_;
+}
+close $in;
+
+$DNA =~ tr/[ATCG]/[TAGC]/;
+$DNA = reverse $DNA;
+
+open(my $out, ">output.txt") or die "error writing";
+print $out $DNA;
