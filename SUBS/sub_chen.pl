c4adc779063eb398f4e24a5e35e170efcdba0fc8
xuchenidea <chnxu@ucdavis.edu>
2017-01-12 10:53:29 -0800

    Add files via upload

--- SUBS/sub_chen.pl
+++ SUBS/sub_chen.pl
@@ -2,18 +2,19 @@
 # sub_chen.pl by chen
 use strict; use warnings;
 
-
 die "usage:perl sub_chen.pl <file>\n" unless @ARGV == 1;
 open IN,"<$ARGV[0]";
-while (<>)  {
-	chomp;
-	my $seq = <IN>;
-	my $motif = <IN>;
-	my ($pos,$now) = (0,-1);
-	until ($pos == -1) {
-		$pos = index ($seq,$motif,$now+1);
-		$now = $pos;
-		print $pos unless $pos < 0;
-	}
+chomp(my $seq = <IN>);
+chomp(my $motif = <IN>);
+
+my $pos = 0;
+my $now = -1;
+my $position;
+
+until ($pos == -1) {
+	$pos = index ($seq,$motif,$now+1);
+	$now = $pos;
+	$position = $pos+1;
+	print "$position\t" unless $pos < 0;
 }
 close IN;
