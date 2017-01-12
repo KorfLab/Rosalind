e0910a9d9eaceb6534d418852a7d08c9b2c2be3f
xuchenidea <chnxu@ucdavis.edu>
2017-01-12 15:33:41 -0800

    Add files via upload

---
+++ FIB/fib_chen.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+# fib_chen.pl  by chen
+use strict;  use warnings;
+
+die "usage:perl fib_chen.pl <file>" unless @ARGV == 1;
+
+open IN,"<$ARGV[0]";
+my @array = split (" ",<IN>,2);
+my $A = 0;
+my $B = 1;
+my $C;
+
+for (my $i=1;$i<$array[0];$i++) {
+	$C = $B+$A*$array[1];
+	$A = $B;
+	$B = $C;
+	}
+
+print $C;
+close IN;
+
