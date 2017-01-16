b51ad543fd7c1d0bfd0e6aa99b99294d1c1c0dfa
xuchenidea <chnxu@ucdavis.edu>
2017-01-16 15:45:25 -0800

    Update iprb_chen.pl

--- IPRB/iprb_chen.pl
+++ IPRB/iprb_chen.pl
@@ -3,17 +3,15 @@
 use strict;  use warnings;
 
 die "perl iprb_chen.pl <file>" unless @ARGV == 1;
-open IN,"<$ARGV[0]";
-my ($k,$m,$n) =  split (" ",<IN>,3);
+open IN, "<$ARGV[0]";
+my ($k, $m, $n) =  split (" ", <IN>, 3);
 
-my $sum = $k+$m+$n;
-print "$k $m $n";
+my $sum = $k + $m+ $n;
 
-# probility aa equal probility Aa2 * (Aa0 + aa0) + aa2 * (Aa1 + aa1)  :
+# probility aa equal:   probility Aa2 * (Aa0 + aa0) + aa2 * (Aa1 + aa1)  
 my $pro_aa = ($m/$sum) *(($m - 1)/($sum - 1) *(1/4)  + $n/($sum - 1)*(1/2)) + ($n/$sum) * ($m/($sum - 1)*(1/2) + (($n - 1)/($sum - 1)*1));
-print $pro_aa;
 
 my $pro_A = 1 - $pro_aa;
-print $pro_A;
+print "$pro_A\n";
 close IN;
 
