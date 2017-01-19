eba38d70b61ae588d8bcd2d862ec7cae6a297df1
stella <srhartono@ucdavis.edu>
2017-01-19 11:53:49 -0800

    Update GC_stella.pl

--- GC/GC_stella.pl
+++ GC/GC_stella.pl
@@ -2,36 +2,69 @@
 
 use strict; use warnings;
 
-my ($input1) = @ARGV;
+sanity_check();
 
-die "usage: $0 <file input>\n" unless @ARGV;
-my %data;
-my $ref; my $dna; 
-open (my $in1, "<", $input1) or die "Cannot read from $input1: $!\n";
+my ($dnaFile) = @ARGV;
+
+my ($perc, $name, $ref, $dna) = (-1);
+
+open (my $in1, "<", $dnaFile) or die "Cannot read from $dnaFile: $!\n";
+
 while (my $line = <$in1>) {
 	chomp($line);
 	if ($line =~ /^>/) {
 		if (defined($dna)) {
-			my ($GC) = $dna =~ tr/Gg/Gg/;
-			($GC) += $dna =~ tr/Cc/Cc/;
-			my $total = length($dna);
-			my $perc = int($GC / $total * 100000000)/1000000;
-			print "$ref\t$perc\t$GC\t$total\n";
+			my $count = count($dna);
+			$name = $ref   if $perc < $count;
+			$perc = $count if $perc < $count;
 		}
-		$dna = "";
-		$ref = $line;
+		undef $dna;
+		($ref) = $line =~ /^>(\w+)$/;
 	}
 	else {
 		$dna .= $line;
 	}
 		
 }
-#this is really ugly code lol
-if (defined($dna)) {
-	my ($GC) = $dna =~ tr/Gg/Gg/;
-	($GC) += $dna =~ tr/Cc/Cc/;
-	my $total = length($dna);
-	my $perc = int($GC / $total * 100000000)/1000000;
-	print "$ref\t$perc\t$GC\t$total\n";
-}
 close $in1;
+
+if (defined($dna)) {
+	my $count = count($dna);
+	$name = $ref   if $perc < $count;
+	$perc = $count if $perc < $count;
+}
+
+print "$name\n$perc\n";
+
+sub count {
+	my ($dna) = @_;
+	my ($GC)  = $dna =~ tr/Gg/Gg/;
+	($GC)    += $dna =~ tr/Cc/Cc/;
+	my $total = length($dna);
+	my $perc  = int($GC / $total * 100000000)/1000000;
+	return($perc);
+}
+
+
+sub sanity_check {
+	die "\nUsage: $0 <file input DNA like below>
+
+#Sample Dataset
+
+>Rosalind_6404
+CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCC
+TCCCACTAATAATTCTGAGG
+>Rosalind_5959
+CCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCT
+ATATCCATTTGTCAGCAGACACGC
+>Rosalind_0808
+CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGAC
+TGGGAACCTGCGGGCAGTAGGTGGAAT
+
+
+#Sample Output
+
+Rosalind_0808
+60.919540\n\n" unless @ARGV;
+
+}
