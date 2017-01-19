795d4bded972fd78ba3af3e8e18ebacec5a04767
xuchenidea <chnxu@ucdavis.edu>
2017-01-19 14:09:13 -0800

    Update hamm_chen.pl

--- HAMM/hamm_chen.pl
+++ HAMM/hamm_chen.pl
@@ -3,20 +3,20 @@
 use strict;  use warnings;
 
 die "usage :perl hamm_chen.pl <file>" unless @ARGV == 1;
-open IN, "<$ARGV[0]";
-chomp (my $seq1 = <IN>) ;
-chomp (my $seq2 = <IN>) ;
+open (my $in, "<",$ARGV[0]);
+chomp (my $seq1 = <$in>) ;
+chomp (my $seq2 = <$in>) ;
 
 my $j = 0;
-my @seq1 = split ("", $seq1);
-my @seq2 = split ("", $seq2);
+my @seq1 = split("", $seq1);
+my @seq2 = split("", $seq2);
 #print "$seq1-------$seq2";
 for (my $i = 0; $i <= length $seq1; $i++) {
-	if (pop @seq1 eq pop @seq2) {
+	if (pop(@seq1) eq pop(@seq2)) {
 	} else {
 		$j ++;
         }
 } 
 print "$j\n";
 
-close IN;
+close $in;
