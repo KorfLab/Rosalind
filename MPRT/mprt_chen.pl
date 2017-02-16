05a4c06e075631c6430c29dfe53ba166302e930b
xuchenidea <chnxu@ucdavis.edu>
2017-02-15 21:56:11 -0800

    Add files via upload

---
+++ MPRT/mprt_chen.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use strict; use warnings;
+use LWP::Simple;
+	
+while(<>) {
+	chomp;
+	my $Fasta = get("http://www.uniprot.org/uniprot/$_.fasta");
+	my @fasta = split("\n",$Fasta,2);
+
+	# find N{P}[ST]{P} motif;
+	print "$fasta[0]\n";
+	for (my $i = 0; $i < length($fasta[1]); $i++) {
+		my $motif = substr($fasta[1], $i, 4);
+		my $sed;
+		if ($motif =~ m/N[^P][ST][^P]/) {$sed = $i+1; print "$sed\t";}
+		else {next;}
+	}
+	print "\n";
+}
+
+
+
