2840fc3e4e80bec6b7ff2c10d46eab55358e7338
stella <srhartono@ucdavis.edu>
2017-02-16 14:41:55 -0800

    Update grph_stella.pl

--- GRPH/grph_stella.pl
+++ GRPH/grph_stella.pl
@@ -4,8 +4,9 @@ use strict; use warnings;
 
 my ($input1) = @ARGV;
 
-die "usage: $0 <file>
+die "usage: $0 <fasta file>
 
+Example:
 >Rosalind_0498
 AAATAAA
 >Rosalind_2391
@@ -16,50 +17,43 @@ TTTTCCC
 AAATCCC
 >Rosalind_5013
 GGGTGGG
-Sample Output
 
+Sample Output:
 Rosalind_0498 Rosalind_2391
 Rosalind_0498 Rosalind_0442
 Rosalind_2391 Rosalind_2323
 
-" unless @ARGV;
+" unless @ARGV == 1;
 
-my %seq; my $ref;
+my %data;
+my ($ref, $beg, $end);
 open (my $in1, "<", $input1) or die "Cannot read from $input1: $!\n";
 while (my $line = <$in1>) {
 	chomp($line);
 	if ($line =~ />/) {
+		if (defined $end) {
+			$data{end}{$end}{$ref} = 1;
+		}
 		$ref = $line;
 		$ref =~ s/>//;
+		undef $beg;
 	}
 	else {
-		$seq{$ref} .= $line;
+		if (not defined($beg)) {
+			($beg) = $line =~ /^(...)/;
+			$data{beg}{$beg}{$ref} = 1;
+		}
+		($end) = $line =~ /(...)$/;
 	}	
 }
 close $in1;
-my %done;
-my %data;my $count = 0;
-foreach my $ref (sort keys %seq) {
-	my $line = $seq{$ref};
-	$count ++;
-	my ($beg) = $line =~ /^(...)/;
-	my ($end) = $line =~ /(...)$/;
-	$data{beg}{$beg}{$ref} = $line;
-	$data{end}{$end}{$ref} = $line;
-}
+$data{end}{$end}{$ref} = 1;
+
 foreach my $beg (sort keys %{$data{beg}}) {
 	foreach my $ref (sort keys %{$data{beg}{$beg}}) {
 		foreach my $ref2 (sort keys %{$data{end}{$beg}}) {
 			next if $ref eq $ref2;
-			if (defined $done{$ref}) {
-				next if defined $done{$ref}{$ref2};
-			}
-			$done{$ref2}{$ref} = 1;
+			print "$ref2 $ref\n";
 		}
 	}
 }
-foreach my $ref (keys %done) {
-	foreach my $ref2 (keys %{$done{$ref}}) {
-		print "$ref $ref2\n";
-	}
-}	
