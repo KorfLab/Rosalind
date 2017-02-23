7e8e1dbce24f89f217ec949fb34a12ef45cc1641
xuchenidea <chnxu@ucdavis.edu>
2017-02-23 10:44:32 -0800

    Add files via upload

---
+++ GRPH/grph_chen.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+# 
+use strict;
+use warnings;
+use FAlite;
+use Data::Dumper;
+
+die "usage: grhp_chen.pl <file.fasta>\n" unless @ARGV == 1;
+my @seq;
+open (my $fh,"< $ARGV[0]") or die;
+my $fasta = new FAlite($fh);
+while (my $entry = $fasta->nextEntry) {
+	push @seq, $entry->seq;
+}
+my %hash;
+
+foreach my $seq (@seq) {
+	foreach my $ref (@seq) {
+			for (my $i = 0; $i < (@seq-3); $i++) {
+				for(my $j = 3; $j < @seq; $i++) {
+					if (substr($seq, $i, 3) eq substr($ref, $j, 3)) {
+						if ($i < $j) {
+							push keys %hash, $seq;
+							push values $hash{$seq},$ref;
+ 						 }
+						else {
+							next;
+						}
+					}
+				}
+
+			}
+		}
+	}
+print Dumper(%hash);
+close $fh;
