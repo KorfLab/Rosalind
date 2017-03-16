b896abe08a82299c1b6963e37575975c8319d41c
xuchenidea <chnxu@ucdavis.edu>
2017-03-16 11:30:42 -0700

    Add files via upload

---
+++ SORT/sort_chen.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use strict;
+use warnings; 
+use Data::Dumper;
+
+my @array;
+my $f;
+while(<>) {
+	my ($chr, $beg, $end) = split;
+	my $length = $end - $beg;
+	my $f = {
+		chr => $chr,
+		length => $length
+	};
+	push @array, $f;
+}
+	my @array_chr = sort {$a->{chr} = $b->{chr}} @array;
+	my @array_len = sort {$a->{length} = $b->{length}} @array;
+	my @array_new = sort {$a->{chr} = $b->{chr} or $a->{beg} = $b->{beg} or $a->{end} = $b->{end}} @array;
+	print Dumper(@array_chr);
+	print Dumper(@array_len);
+	print Dumper(@array_new);
+	
+	print Dumper(@array_new);
