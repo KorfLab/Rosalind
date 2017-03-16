d675141c825c46073d96f389e8342560e2288c0c
xuchenidea <chnxu@ucdavis.edu>
2017-03-16 12:02:17 -0700

    Add files via upload

--- SORT/sort_chen.pl
+++ SORT/sort_chen.pl
@@ -10,13 +10,26 @@ while(<>) {
 	my $length = $end - $beg;
 	my $f = {
 		chr => $chr,
+		end => $end,
+		beg => $beg,
 		length => $length
 	};
 	push @array, $f;
 }
-	my @array_chr = sort {$a->{chr} = $b->{chr}} @array;
-	my @array_len = sort {$a->{length} = $b->{length}} @array;
-	my @array_new = sort {$a->{chr} = $b->{chr} or $a->{beg} = $b->{beg} or $a->{end} = $b->{end}} @array;
-	print Dumper(@array_chr);
-	print Dumper(@array_len);
-	print Dumper(@array_new);
+
+my @array_chr = sort {$a->{chr} = $b->{chr}} @array;
+my @array_len = sort {$a->{length} = $b->{length}} @array;
+my @array_new = sort {$a->{chr} = $b->{chr} or $a->{beg} = $b->{beg} or $a->{end} = $b->{end}} @array;
+
+print "sort chr:\n";
+foreach my $k (@array_chr) {
+	print "k->{chr}\t$k->{length}\t$k->{beg}\t$k->{end}\n";
+}
+print"sort length:\n";
+foreach my $k (@array_len) {
+	print "$k->{chr}\t$k->{length}\t$k->{beg}\t$k->{end}\n";
+}
+print"sort all:\n";
+foreach my $k (@array_new) {
+	print "$k->{chr}\t$k->{length}\t$k->{beg}\t$k->{end}\n";
+}
